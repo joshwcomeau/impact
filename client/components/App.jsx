88ae506b2890e91abc3dd9f5cd330c569f1d15c8
Josh Comeau <josh@breather.com>
2015-11-26 18:25:49 -0500

    Integrated collections, used tracker-react for nice reactivity. Started playing around with additional components.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,28 +1,8 @@
 App = React.createClass({
-  getPosts() {
-    return [
-      {
-        _id: 1,
-        title: "A Brand New Day!",
-        text: "Thanks so much for this warm welcome. I can't wait to introduce you to this blogging engine!",
-        createdAt: moment().subtract(1, 'hour').toDate(),
-      }, {
-        _id: 2,
-        title: "New Feature - Posts",
-        text: "As of now, this blog features actual posts. Stay tuned!",
-        createdAt: moment().subtract(30, 'minutes').toDate(),
-      }, {
-        _id: 3,
-        title: "Holy cow, progress is fast!",
-        text: "In other news, React is cool. Back to you in the studio, John.",
-        createdAt: moment().subtract(3, 'minutes').toDate(),
-
-      }
-    ]
-  },
+  mixins: [TrackerReact],
 
   renderPosts() {
-    return this.getPosts().map( post => <BlogPost key={post._id} post={post} /> );
+    return BlogPosts.find({}).fetch().map( post => <BlogPost key={post._id} post={post} /> );
   },
 
   render() {
@@ -32,6 +12,8 @@ App = React.createClass({
         <ul>
           {this.renderPosts()}
         </ul>
+        <h4>Add New Post:</h4>
+        <BlogPostForm />
       </div>
     );
   }
